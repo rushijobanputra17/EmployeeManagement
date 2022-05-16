4aa8945da4b8377e2d47a4b48f2e28b69ddeda42
User32 <User32@PC-32>
2022-05-16 15:49:39 +0530

    update in project controller

--- Backend/EmployeeManagement/Controllers/ProjectController.cs
+++ Backend/EmployeeManagement/Controllers/ProjectController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataAccessLayer.Services;
+using EmployeeManagement.CommonFunctions;
+using EmployeeManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +12,28 @@ namespace EmployeeManagement.Controllers
 {
     public class ProjectController : ApiController
     {
-        
+        public static ProjectRepository projectRepository = new ProjectRepository();
+        public JsonResponse objResponse;
+
+        public JsonResponse GetProjects(bool isActive)
+        {
+            try
+            {
+                var projects = projectRepository.GetAllProject(isActive);
+                objResponse = UtilityFunctions.GetJsonResponse(1, "Records found : " + projects.Count(), projects);
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.InnerException.Message);
+                }
+                else
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.Message);
+                }
+            }
+            return objResponse;
+        }
     }
 }
