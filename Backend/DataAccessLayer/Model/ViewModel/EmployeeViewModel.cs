dc306d574ae7abb1f4878ffd03c15130cd227ce8
harsh-mungra <harshmungra18112000@gmail.com>
2022-05-16 15:56:12 +0530

    Insert employee method created in Employee API

---
+++ Backend/DataAccessLayer/Model/ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Model.ViewModel
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; } = 0;
+
+        private string _Name;
+        [Required]
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if(!string.IsNullOrEmpty(value))
+                {
+                    _Name = value.Trim();
+                }
+            }
+        }
+
+        public int? DeptId { get; set; }
+
+        [Required]
+        public System.DateTime JoiningDate { get; set; }
+
+        [Required]
+        public decimal Salary { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
--- Backend/EmployeeManagement/Controllers/EmployeeController.cs
+++ Backend/EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Http;
+using AutoMapper;
+using DataAccessLayer.Model;
+using DataAccessLayer.Model.ViewModel;
 using DataAccessLayer.Services;
 using EmployeeManagement.CommonFunctions;
 using EmployeeManagement.Models;
@@ -11,6 +14,10 @@ namespace EmployeeManagement.Controllers
     public class EmployeeController : ApiController
     {
         public static EmployeeRepository employeeRepository = new EmployeeRepository();
+
+        public static AutoMapper.MapperConfiguration config = new AutoMapper.MapperConfiguration(cgf => cgf.CreateMap<EmployeeViewModel, Employee>());
+        public static Mapper mapper = new Mapper(config);
+
         public JsonResponse objResponse;
 
         public JsonResponse GetEmployees(bool isActive)
@@ -22,7 +29,7 @@ namespace EmployeeManagement.Controllers
             }
             catch (Exception ex)
             {
-                if(ex.InnerException != null)
+                if (ex.InnerException != null)
                 {
                     objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.InnerException.Message);
                 }
@@ -56,9 +63,32 @@ namespace EmployeeManagement.Controllers
         }
 
         [HttpPost]
-        public JsonResponse InsertEmployee()
+        public JsonResponse InsertEmployee(EmployeeViewModel model)
         {
-            
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    employeeRepository.InsertUpdateEmployee(mapper.Map<Employee>(model));
+                    objResponse = UtilityFunctions.GetJsonResponse(1, "Record added successfully", model);
+                }
+                else
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(0, "Some input is invalid", ModelState.Values.SelectMany(v => v.Errors).Select(v => v.ErrorMessage).ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.InnerException.Message);
+                }
+                else
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.Message);
+                }
+            }
+            return objResponse;
         }
     }
 }
