394a95b83d808a5f1c64b288dea3a73b8dd363b3
harsh-mungra <harshmungra18112000@gmail.com>
2022-05-16 14:55:00 +0530

    Common Function for creating Json Response added

---
+++ Backend/EmployeeManagement/CommonFunctions/CommonFunctions.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.CommonFunctions
+{
+    public class UtilityFunctions
+    {
+        public static JsonResponse GetJsonResponse(int responseStatus, string message, dynamic result)
+        {
+            return new JsonResponse()
+            {
+                ResponseStatus = responseStatus,
+                Message = message,
+                Result = result
+            };
+        }
+    }
+}
