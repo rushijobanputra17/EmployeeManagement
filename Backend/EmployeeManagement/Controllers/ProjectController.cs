eee56a0b97c346571377be90ee9a3c7390e761ec
User32 <User32@PC-32>
2022-05-16 16:15:48 +0530

    update project controller

--- Backend/EmployeeManagement/Controllers/ProjectController.cs
+++ Backend/EmployeeManagement/Controllers/ProjectController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Services;
+﻿using AutoMapper;
+using DataAccessLayer.Model;
+using DataAccessLayer.Services;
 using EmployeeManagement.CommonFunctions;
 using EmployeeManagement.Models;
 using System;
@@ -14,6 +16,8 @@ namespace EmployeeManagement.Controllers
     {
         public static ProjectRepository projectRepository = new ProjectRepository();
         public JsonResponse objResponse;
+        public static AutoMapper.MapperConfiguration config = new AutoMapper.MapperConfiguration(cgf => cgf.CreateMap<ProjectViewModel, Project>());
+        public static Mapper mapper = new Mapper(config);
 
         public JsonResponse GetProjects(bool isActive)
         {
@@ -24,6 +28,35 @@ namespace EmployeeManagement.Controllers
             }
             catch (Exception ex)
             {
+                if (ex.InnerException != null)
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.InnerException.Message);
+                }
+                else
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.Message);
+                }
+            }
+            return objResponse;
+        }
+
+        public JsonResponse InsertProject(ProjectViewModel project)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    projectRepository.InsertUpdateProject(mapper.Map<Project>(project));
+                    objResponse = UtilityFunctions.GetJsonResponse(1, "Record added successfully", project);
+                }
+                else
+                {
+                    objResponse = UtilityFunctions.GetJsonResponse(0, "Some input is invalid", ModelState.Values.SelectMany(v => v.Errors).Select(v => v.ErrorMessage).ToList());
+                }
+            }
+            catch (Exception ex)
+            {
+
                 if (ex.InnerException != null)
                 {
                     objResponse = UtilityFunctions.GetJsonResponse(2, "Exception", ex.InnerException.Message);
---
+++ Backend/EmployeeManagement/Models/ProjectViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagement.Models
+{
+    public class ProjectViewModel
+    {
+
+        int? Id { get; set; }
+
+        string Name { get; set; }   
+
+        string Description { get; set; }    
+
+        DateTime StartDate { get; set; }    
+
+        string Status { get; set; } 
+
+        Boolean IsActive { get; set; }  
+
+    }
+}
