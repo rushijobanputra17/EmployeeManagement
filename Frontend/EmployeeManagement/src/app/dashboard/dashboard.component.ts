5d81672eb7d7ae5dceba6884ebdf3e94e117fa71
prince <97102549+princepatel74@users.noreply.github.com>
2022-05-17 11:30:57 +0530

    commit

--- Frontend/EmployeeManagement/src/app/dashboard/dashboard.component.ts
+++ Frontend/EmployeeManagement/src/app/dashboard/dashboard.component.ts
@@ -35,6 +35,45 @@ export class DashboardComponent implements OnInit {
   }
 
   handleOk(){
+    
+    this.showLoader = true;
+    debugger
+    const token =sessionStorage.getItem("logged_user")
+    if(token==null){
+      this.message.error("Login Required", {
+        nzDuration: 1000
+      });
+      this.router.navigate(['/login']);
+    }
+
+    else{
+      const ProjectDetail = this.validateForm.controls
+      this.httpService.ProjectBody.Name = ProjectDetail["Name"].value;
+      this.httpService.ProjectBody.Description = ProjectDetail["Description"].value;
+      this.httpService.ProjectBody.StartDate = ProjectDetail["datePicker"].value;
+      this.httpService.ProjectBody.Status = ProjectDetail["status"].value;
+      this.httpService.ProjectBody.IsActive = ProjectDetail["active"].value;
+      this.httpService.AddProjectData().subscribe(
+        (response) => {
+          console.log(response);
+          this.showLoader = false;
+          this.message.success("Project Added Successfully!", {
+            nzDuration: 1000
+          });
+          this.isVisible = false;
+          this.validateForm.reset();
+          this.showLoader = false;
+        },
+        (error: any) => {
+          this.message.error("Server Error! Please Reload Your Page", {
+            nzDuration: 5000
+          });
+          this.showLoader = false;
+        },
+        () => console.log("done")
+      );
+    }
+  
   }
 
   DeleteProduct(Data:any){
